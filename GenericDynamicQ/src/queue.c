1c6069f1e81091a7ce26ee647ce47a8fa8159694
pino3567 <pino3567@localhost>
2020-10-23 00:07:29 -0700

    Queue Makefile Setup

---
+++ GenericDynamicQ/src/queue.c
@@ -0,0 +1,9 @@
+/**************************************************************************
+ File name: 	queue.c
+ Author:   	 	Kailani Pinon
+ Date:				10.22.2020
+ Class:				CS300
+ Assignment:	Generic Dynamic Queue
+ Purpose:			This file implements the Queue ADT from queue.h
+ *************************************************************************/
+
---
+++ GenericDynamicQ/src/queuedriver.c
@@ -0,0 +1,13 @@
+/**************************************************************************
+ File name:			queuedriver.c
+ Author:        Kailani Pinon
+ Date:          10.22.2020
+ Class:         CS300
+ Assignment:    Dynamic Queue
+ Purpose:       This file implements the Queue ADT from queue.h
+ *************************************************************************/
+
+int main ()
+{
+
+}
